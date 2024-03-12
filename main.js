3c4885ca3b03b2b2e12685fc037dc004e6246081
mathiaswarkenti <162718996+mathiaswarkenti@users.noreply.github.com>
2024-03-12 11:23:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-aUi_Ae70aD82udSpH82VLleO9KKtQgNa5Q&s";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsm7smVGUaWn-6umRg-mOCV_CjaJMUBEE7sA&s";
+}
